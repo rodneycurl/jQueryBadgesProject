1b6ceb9412300e27605a28e4e6e151aa86f75411
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-11-19 12:29:01 -0500

    Made AJAX request to CodeSchool.com API and displayed the response in the DOM

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/rodneycurl.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
+
+  function addCourses(courses) {
+    let $badges = $('#badges');
+    courses.forEach(function(course) {
+      let $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+
+    })
+  }
 
 });
